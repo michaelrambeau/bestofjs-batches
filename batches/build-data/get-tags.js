12276c93bc3b05e47ccaf795932f7678a13b7340
Michael Rambeau <mikeair@gmail.com>
2015-09-26 06:22:58 +0900

    Sort tag list by name

--- batches/build-data/get-tags.js
+++ batches/build-data/get-tags.js
@@ -2,9 +2,11 @@
 function getTags(options, cb) {
   var model = options.Tag;
   if (!model) return cb(new Error('No `Tag` model passed to options object!'));
-  model.find({}).exec(function (err, docs) {
-    if (err) return cb(err);
-    cb(null, docs);
-  });
+  model.find({})
+    .sort({'name': 1})
+    .exec(function (err, docs) {
+      if (err) return cb(err);
+      cb(null, docs);
+    });
 }
 module.exports = getTags;
