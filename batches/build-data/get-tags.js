49bc2650f7e2b1909a7ecd2c2e699488c8206dc2
Michael Rambeau <mikeair@gmail.com>
2015-10-25 14:41:32 +0900

    Optimization: remove tag _id field that is no more used by the web-ui app

--- batches/build-data/get-tags.js
+++ batches/build-data/get-tags.js
@@ -2,7 +2,11 @@
 function getTags(options, cb) {
   var model = options.Tag;
   if (!model) return cb(new Error('No `Tag` model passed to options object!'));
-  var fields = 'code name';
+  var fields = {
+    code: 1,
+    name: 1,
+    _id: 0//required to omit _id field
+  };
   model.find({}, fields)
     .sort({'name': 1})
     .exec(function (err, docs) {
